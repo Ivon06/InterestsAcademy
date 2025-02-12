08115a8e082a713324d53802e6dc028494ed4ed8
Neda <npetrova2002@icloud.com>
2025-02-12 15:46:23 +0200

    Added navbar dropdown and card functionality

--- InterestsAcademy/wwwroot/js/teamcards.js
+++ InterestsAcademy/wwwroot/js/teamcards.js
@@ -1,66 +1,95 @@
 ﻿var sliderCards = (function (document, $) {
+    "use strict";
 
-    'use strict';
-
-    var $sliderCards = $('.slider--cards'),
-        $list = $('#card-list'),
-        $listItems = $('#card-list li'),
+    var $sliderCards = $(".slider--cards"),
+        $list = $("#card-list"),
+        $listItems = $("#card-list li"),
         $nItems = $listItems.length,
-        $nView = 3,
+        $nView = 3, // Number of visible cards
         autoSlider,
         $current = 0,
         $isAuto = true,
         $autoTime = 2500,
+        $cardWidth = 220, // Width of each card
+        $startPos = 0,
+        $dragging = false;
 
-        _init = function () {
-            _setWidth();
-            _eventInit();
-        },
+    function _init() {
+        _duplicateCards();
+        _setWidth();
+        _eventInit();
+    }
 
-        _setWidth = function () {
-            $list.css({
-                'width': ~~(100 * ($nItems / $nView)) + '%'
-            });
-            $listItems.css('width', 100 / $nItems + '%');
-            $sliderCards.css({ opacity: 1 });
-        },
+    // Duplicate first few cards at the end for infinite looping effect
+    function _duplicateCards() {
+        let cloneFirst = $listItems.slice(0, $nView).clone();
+        $list.append(cloneFirst);
+        $listItems = $("#card-list li"); // Update item count after cloning
+        $nItems = $listItems.length;
+    }
 
-        _eventInit = function () {
+    function _setWidth() {
+        $list.css({
+            "width": ~~(100 * ($nItems / $nView)) + "%"
+        });
+        $listItems.css("width", 100 / $nItems + "%");
+        $sliderCards.css({ opacity: 1 });
+    }
 
-            $.each($listItems, function (i) {
-                var $this = $(this);
-                $this.on('click', function (e) {
-                    e.preventDefault();
-                    _stopAuto(i);
-                    _moveTo($this, i);
-                });
-            });
+    function _eventInit() {
+        // Enable dragging functionality
+        $sliderCards.on('mousedown touchstart', function (e) {
+            e.preventDefault();
+            $dragging = true;
+            $startPos = e.pageX || e.originalEvent.touches[0].pageX; // Get initial drag position
+            $sliderCards.css('cursor', 'grabbing'); // Change cursor to grabbing when dragging
+        });
 
-            autoSlider = setInterval(_autoMove, $autoTime);
-        },
-
-        _moveTo = function (obj, index) {
-
-            obj.find('.card').addClass('active');
-            $listItems.not(obj).find('.card').removeClass('active');
-
-            $list.css({
-                transform: `translateX(${-(index * 220)}px)`
-            });
-        },
-
-        _autoMove = function () {
-            if ($isAuto) {
-                $current = ($current + 1) % $nItems;
+        $(document).on('mousemove touchmove', function (e) {
+            if ($dragging) {
+                var move = (e.pageX || e.originalEvent.touches[0].pageX) - $startPos; // Calculate drag movement
+                $list.css('transform', `translateX(${move}px)`); // Move the slider based on drag distance
             }
-            _moveTo($listItems.eq($current), $current);
-        },
+        });
 
-        _stopAuto = function (index) {
-            clearInterval(autoSlider);
-            $isAuto = false;
-            _autoMove(index);
-        };
+        $(document).on('mouseup touchend', function () {
+            if ($dragging) {
+                var finalPos = $list.position().left; // Final position after drag
+                var newIndex = Math.round(Math.abs(finalPos) / $cardWidth); // Calculate the closest card
+                _moveTo(newIndex);
+                $dragging = false;
+                $sliderCards.css('cursor', 'grab'); // Change cursor back
+            }
+        });
+
+        autoSlider = setInterval(_autoMove, $autoTime); // Initial auto-slider
+    }
+
+    function _moveTo(index) {
+        $list.css({
+            transition: "transform 0.5s ease-in-out",
+            transform: `translateX(${-(index * $cardWidth)}px)`
+        });
+
+        // If we reach the last cloned item, instantly reset to the original
+        if (index >= $nItems - $nView) {
+            setTimeout(() => {
+                $list.css({
+                    transition: "none",
+                    transform: `translateX(0px)`
+                });
+            }, 500);
+            $current = 0;
+        } else {
+            $current = index;
+        }
+    }
+
+    function _autoMove() {
+        if ($isAuto) {
+            _moveTo($current + 1);
+        }
+    }
 
     return {
         init: _init
@@ -68,9 +97,10 @@
 
 })(document, jQuery);
 
-$(window).on('load', function () {
+$(window).on("load", function () {
     sliderCards.init();
 });
+
 $(window).scroll(function () {
     if ($(this).scrollTop() > 50) {
         $(".navv").addClass("scrolled");
@@ -78,4 +108,3 @@ $(window).scroll(function () {
         $(".navv").removeClass("scrolled");
     }
 });
-
