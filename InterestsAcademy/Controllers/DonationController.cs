71d7ee173d6a3beab47344f2213a43afdb41e2a8
Ivon06 <ivonmircheva2@gmail.com>
2025-02-14 12:55:44 +0200

    Dination functionality startded

---
+++ InterestsAcademy/Controllers/DonationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterestsAcademy.Controllers
+{
+    public class DonationController : Controller
+    {
+
+        public async Task<IActionResult> Categories()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
